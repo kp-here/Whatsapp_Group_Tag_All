111e14b27a747382947257a1c1039ab37099c633
kp <abhiramjuly16@gmail.com>
2021-11-22 20:22:38 +0530

    hello

--- tagall.py
+++ tagall.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup as bs
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-#okayy
-
+#hello
 # grp_name = input("\nEnter Group Name :\n")
 grp_name="Kozhikoood"
 
@@ -38,4 +38,6 @@ group_title.send_keys(grp_name + Keys.ENTER)
 
 #clicked on group name header
 head_title= findbyxpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[4]/div[1]/header[1]/div[2]/div[1]/div[1]/span[1]")
+
+
 
