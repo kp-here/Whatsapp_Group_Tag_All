3bb6faf37248886d62b0636ef7f8432b276a1f8b
kp <abhiramjuly16@gmail.com>
2021-11-23 06:37:06 +0530

    added contactlist import and duplicate remove and tag i grp using Keys - bugs are there

--- tagall.py
+++ tagall.py
@@ -8,14 +8,14 @@ import time
 
 #hello
 # grp_name = input("\nEnter Group Name :\n")
-grp_name="Kozhikoood"
+grp_name="kozhikoood"
 
 #importing contact names
 
 
 #loading web driver
 driver = webdriver.Chrome(executable_path=r'T:\Whatsapp Automation\chromedriver.exe')
-wait = WebDriverWait(driver, 5)
+wait = WebDriverWait(driver, 20)
 driver.maximize_window()
 driver.get('https://web.whatsapp.com/')
 
@@ -30,7 +30,7 @@ def findbyxpath_noclick(x):
     return wait.until(EC.presence_of_element_located((By.XPATH, x)))
 
 #click search box
-group_title = WebDriverWait(driver, 600).until(EC.presence_of_element_located((By.XPATH, "//div[@class='_13NKt copyable-text selectable-text']")))
+group_title = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='_13NKt copyable-text selectable-text']")))
 group_title.click()
 
 #type in group name and select it
@@ -39,13 +39,47 @@ group_title.send_keys(grp_name + Keys.ENTER)
 #clicked on group name header
 head_title= findbyxpath("/html[1]/body[1]/div[1]/div[1]/div[1]/div[4]/div[1]/header[1]/div[2]/div[1]/div[1]/span[1]")
 
-time.sleep(2)
 
-page_s=driver.page_source
-soup = bs( page_s , 'html.parser')
+#view all
+try:
+    view_all= WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH,"/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[3]/span[1]/div[1]/span[1]/div[1]/div[1]/section[1]/div[6]/div[2]/button[1]/div[2]")))
+except:
+    print("error occured")
+else:
+    view_all.click()
+finally:
+    time.sleep(2)
+    page_s=driver.page_source
+    soup = bs( page_s , 'html.parser')
 
-a= soup.find( class_ =  "body")
-#"emoji-texttt _ccCW FqYAR i0jNr"
-for i in a:
-    print(a.string)
+    l=set()
+
+    for i in soup.find_all(class_="_3Bc7H KPJpj"):
+        for j in i.find_all(class_="emoji-texttt _ccCW FqYAR i0jNr"): 
+            l.add(j.text)
+    
+    l.remove("You")
+    k=list(l)
+
+    with open('a.txt','w',encoding="utf-8") as f:
+        for i in k:
+            f.write(i+"\n")
+    f.close()
+
+
+    try:
+        #cross button
+        cross_button=WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH,"/html[1]/body[1]/div[1]/div[1]/span[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/header[1]/div[1]/div[1]/button[1]")))
+        
+    except:
+        print("error occured")
+    else:
+        cross_button.click()
+        
+    finally:
+        #messagebox
+        type_box=findbyxpath("//div[@class='_1UWac _1LbR4']//div[@class='_13NKt copyable-text selectable-text']")
+        for i in k:
+            type_box.send_keys("@"+ i + Keys.ENTER)
+        type_box.send_keys(Keys.ENTER) 
 
